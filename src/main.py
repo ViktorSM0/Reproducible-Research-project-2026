672c849b600f9bde8c4eda9ca30fa004358bc325
Viktor Senkiv <senkiv500@gmail.com>
2026-05-31 19:02:21 +0200

    Add the word simulation file and output images

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from protolang_simulation import ProtolanguageSimulation
 from adaptive_dynamics_simulation import AdaptiveDynamicsSimulation
+from word_formation_simulation  import WordFormationSimulation
 
 if __name__ == "__main__":
     proto_experiment = ProtolanguageSimulation(n_pop=100, n_objects=5, n_signals=5, sample_size=12)
@@ -7,3 +8,6 @@ if __name__ == "__main__":
 
     adaptive_experiment = AdaptiveDynamicsSimulation(alpha=2.5, n_objects=10, n_sounds=20)
     adaptive_experiment.run_simulation_and_plot(total_mutations=50000)
+
+    sim = WordFormationSimulation(n_objects=100, epsilon=0.2)
+    sim.run_simulation_and_plot()
